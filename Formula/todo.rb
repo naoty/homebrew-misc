06f4ea4635c6f50487176d6f4f0180f28b5c0e4e
Naoto Kaneko <naoty.k@gmail.com>
2017-02-20 13:56:18 +0900

    Fix todo formula to add empty file
    When the only "todo" file is installed, Homebrew causes "Empty
    installation" error because "todo" file is handled as a metafile.

--- Formula/todo.rb
+++ Formula/todo.rb
@@ -2,9 +2,13 @@ class Todo < Formula
   desc "A todo management tool just for myself"
   homepage "https://github.com/naoty/todo"
   url "https://github.com/naoty/todo/releases/download/0.2.0/todo.tar.gz"
-  sha256 "0f7d4268b36c0b7e33ce0e7d1ae387d9db30f00332c552c70404798b7b212256"
+  sha256 "be20e4069c0ae49998dfc00a010ca8f5d49d26193bd0c3e8611a4bf53cac469d"
 
   def install
-    bin.install "bin/todo"
+    bin.install "todo"
+
+    # Avoid "Empty installation" error which will be caused when the only
+    # "todo" file is installed.
+    bin.install "empty"
   end
 end
