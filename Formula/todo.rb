04b215fb61ef909c5c37431ef97d3d781bcb0b0e
Naoto Kaneko <naoty.k@gmail.com>
2017-02-28 10:01:52 +0900

    Remove a workaround in the formula for todo
    Because Homebrew has been fixed.

--- Formula/todo.rb
+++ Formula/todo.rb
@@ -6,9 +6,5 @@ class Todo < Formula
 
   def install
     bin.install "todo"
-
-    # Avoid "Empty installation" error which will be caused when the only
-    # "todo" file is installed.
-    bin.install "empty"
   end
 end
