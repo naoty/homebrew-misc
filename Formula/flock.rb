c9b2d31d521547493120238bc42469effd30a402
Naoto Kaneko <naoty.k@gmail.com>
2017-03-09 17:42:03 +0900

    Add a formula for flock

---
+++ Formula/flock.rb
@@ -0,0 +1,10 @@
+class Flock < Formula
+  desc "Object graph in Swift"
+  homepage "https://github.com/naoty/flock"
+  url "https://github.com/naoty/flock/releases/download/0.1.0/flock"
+  sha256 "30b758fa2b4f24f2b01686b8bc0f927562247d8e82b291a47aba7edd7bddb5a5"
+
+  def install
+    bin.install "flock"
+  end
+end
--- README.md
+++ README.md
@@ -8,5 +8,6 @@ $ brew tap naoty/misc
 ```
 
 ## Formulae
+* [flock](https://github.com/naoty/flock): Object graph in Swift
 * [table](https://github.com/naoty/table): A command to print ASCII table from stdin
 * [todo](https://github.com/naoty/todo): A todo management tool just for myself
