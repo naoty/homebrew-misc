715a3e2127b62c92391c800ccd97917eb0f4ccdf
Naoto Kaneko <naoty.k@gmail.com>
2017-03-13 15:05:32 +0900

    Fix the formula for flock to build the executable

--- Formula/flock.rb
+++ Formula/flock.rb
@@ -1,10 +1,16 @@
 class Flock < Formula
   desc "Object graph in Swift"
   homepage "https://github.com/naoty/flock"
-  url "https://github.com/naoty/flock/releases/download/0.1.0/flock"
-  sha256 "30b758fa2b4f24f2b01686b8bc0f927562247d8e82b291a47aba7edd7bddb5a5"
+  url "https://github.com/naoty/flock/archive/0.1.0.tar.gz"
+  sha256 "0e5d874fc7207037a4f0bd7e7be398a5c07a27e65ff3a74f05d0cb197f202430"
+
+  depends_on :xcode => ["8.0", :build]
 
   def install
-    bin.install "flock"
+    ENV.delete("CC")
+    ENV["SDKROOT"] = MacOS.sdk_path
+    system "swift", "build", "-c", "release"
+    bin.install ".build/release/flock"
+    lib.install Dir[".build/release/*.dylib"]
   end
 end
