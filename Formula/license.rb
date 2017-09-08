cb027153c73cd441b76914d5948744b34f4f7125
Naoto Kaneko <naoty.k@gmail.com>
2017-09-08 14:56:18 +0900

    Update a formula for naoty/license

--- Formula/license.rb
+++ Formula/license.rb
@@ -1,8 +1,8 @@
 class License < Formula
   desc "A command to generate LICENSE file"
   homepage "https://github.com/naoty/license"
-  url "https://github.com/naoty/license/releases/download/0.1.0/license.tar.gz"
-  sha256 "11854074dce9f7941a6f53306d23fc3a3a64da4b31a94954f00806202f0c9259"
+  url "https://github.com/naoty/license/releases/download/0.1.1/license.tar.gz"
+  sha256 "050cfb85006b1238f4d2c51f82ad52b212e852ff1292796e2706b9e7ab532c4f"
 
   def install
     bin.install "license"
