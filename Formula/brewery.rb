a57acb6dafb0eeae254aaa3abf710b4cf0c60018
Naoto Kaneko <naoty.k@gmail.com>
2017-09-17 11:26:22 +0900

    Update a formula for brewery

--- Formula/brewery.rb
+++ Formula/brewery.rb
@@ -1,8 +1,8 @@
 class Brewery < Formula
   desc     "A command to generate a formula for Homebrew"
   homepage "https://github.com/naoty/brewery"
-  url      "https://github.com/naoty/brewery/releases/download/0.1.0/brewery.tar.gz"
-  sha256   "46622383fafa4bf73793f224789db91bb73967c9f98b262ac8ae4ee9508337e9"
+  url      "https://github.com/naoty/brewery/releases/download/0.1.1/brewery.tar.gz"
+  sha256   "f894bb7ab32407e284bc6b59f6366aefcc4be8b5a1139fa24cc1d6e8a1297d6a"
 
   def install
     bin.install "brewery"
