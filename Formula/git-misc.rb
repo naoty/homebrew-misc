879ffe9a53ed1b28f504f917fbf9890abc5d4714
Naoto Kaneko <naoty.k@gmail.com>
2017-09-26 12:30:59 +0900

    Update a formula for naoty/git-misc

--- Formula/git-misc.rb
+++ Formula/git-misc.rb
@@ -1,8 +1,8 @@
 class GitMisc < Formula
   desc     "Misc scripts for git"
   homepage "https://github.com/naoty/git-misc"
-  url      "https://github.com/naoty/git-misc/releases/download/0.2.0/git-misc.tar.gz"
-  sha256   "3e45f4ff7c5756bb4a68648e5719939ab4a1f6b2696e7bcc9c7546ac9911ae26"
+  url      "https://github.com/naoty/git-misc/releases/download/0.2.1/git-misc.tar.gz"
+  sha256   "0a7da85b68ba099401e560a3aa06e566680ed44fd866a9d97f84896c3db48991"
 
   def install
     bin.install Dir["git-*"]
