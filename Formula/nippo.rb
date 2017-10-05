f13bc3decb96a9b6c6b83e0f6e9400b08132566f
Naoto Kaneko <naoty.k@gmail.com>
2017-10-05 22:45:55 +0900

    Update nippo

--- Formula/nippo.rb
+++ Formula/nippo.rb
@@ -1,8 +1,8 @@
 class Nippo < Formula
   desc     "日報作成スクリプト"
   homepage "https://github.com/naoty/nippo"
-  url      "https://github.com/naoty/nippo/releases/download/0.1.0/nippo.tar.gz"
-  sha256   "6c09bf2025ba736cbd159effbb6db5243fd7d0dd3943b78434b64411d8baf49f"
+  url      "https://github.com/naoty/nippo/releases/download/0.2.0/nippo.tar.gz"
+  sha256   "e45886dcfa51c83a56685363eebf5c6e273d01a007e866b9ee2b4d17f2ad5d47"
 
   def install
     bin.install "nippo"
