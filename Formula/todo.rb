86f1f6c3665785a31b7bbd4398a69a6b4e2695e8
Naoto Kaneko <naoty.k@gmail.com>
2017-12-21 17:50:37 +0900

    Update a new formula for todo

--- Formula/todo.rb
+++ Formula/todo.rb
@@ -1,8 +1,8 @@
 class Todo < Formula
   desc "A todo management tool just for myself"
   homepage "https://github.com/naoty/todo"
-  url "https://github.com/naoty/todo/releases/download/0.3.0/todo.tar.gz"
-  sha256 "62854affe501c2cce250ed1a68d401879789d1571295c365fa869e3ce08a7d50"
+  url "https://github.com/naoty/todo/releases/download/0.3.1/todo.tar.gz"
+  sha256 "ef25c7e90b23c82a174ffafe394c16861ee05d688ff43444d8192299543b0fb1"
 
   def install
     bin.install "todo"
