8728bf0d93d3ce5fbe1c63dd751e45d011ed571e
Naoto Kaneko <naoty.k@gmail.com>
2020-07-11 20:54:02 +0900

    Update a formula for table 0.3.0

--- Formula/table.rb
+++ Formula/table.rb
@@ -1,11 +1,10 @@
 class Table < Formula
   desc "A command to print ASCII table from stdin"
   homepage "https://github.com/naoty/table"
-  url "https://github.com/naoty/table/releases/download/0.2.0/table.tar.gz"
-  sha256 "5a75b10f23a109966f4bf15aca63830089c923e8f8127b20b7ceb0be32ecbe24"
+  url "https://github.com/naoty/table/releases/download/0.3.0/table-0.3.0-x86_64-apple-darwin.tar.gz"
+  sha256 "76b35c34c8a7ef8304bf56077a2bfa5b015a4934b6b871d9ce41f1fd3c6dc5ea"
 
   def install
     bin.install "table"
   end
 end
-
